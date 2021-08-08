77daa0db0b8d4b98c10ed3de0b230bb84af78777
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-08-08 14:45:57 +0300

    1.0

--- index.js
+++ index.js
@@ -6,13 +6,33 @@ const colors = [
     '#009688',
     '#795548',
 ];
+let timerId = null;
+
 
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const refs = {
-    startButton: document.querySelector('[data - action="start"]'),
-    stopButton: document.querySelector('[data - action="stop"]'),
+    startButton: document.querySelector('[data-action="start"]'),
+    stopButton: document.querySelector('[data-action="stop"]'),
     allScreen: document.querySelector('body')
+};
+
+refs.startButton.addEventListener('click', onStart);
+refs.stopButton.addEventListener('click', onStop);
+
+function onStart() {
+    timerId = setInterval(paintHandler, 1000);
+    refs.startButton.removeEventListener('click', onStart);
+};
+
+function onStop() {
+    clearInterval(timerId);
+    refs.startButton.addEventListener('click', onStart);
+};
+
+function paintHandler() {
+    const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    refs.allScreen.style.backgroundColor = color;
 }
